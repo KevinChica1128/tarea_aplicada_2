57ca103e857b84788af2df70ae35a0602f98ba62
KevinChica1128 <kevin.chica@correounivalle.edu.co>
2018-03-02 13:15:41 -0500

    listo, empezar beamer

--- Tarea2.R
+++ Tarea2.R
@@ -8,11 +8,24 @@ sample(1:87,12) #Me genera 12 números aleatorios, correspondientes a los hombres
 empleados1H<-empleados1H[c(10,29,9,13,47,45,2,79,69,15,41,3),] #Muestra de 12 hombres obtenida
 empleados1<-rbind(empleados1H,empleadis1M) #Me pone en una misma base de datos los hombres y la mujeres, esta será nuestra base de datos final.
 
-#punto 1:
-modelo<-lm(empleados1$Altura~empleados1$Peso)
+#punto 1 y 2:
+modelo<-lm(empleados1$Peso~empleados1$Altura)
 summary(modelo)
 
-#punto 4:
+#punto 3:
+sigma2<-summary(modelo)$sigma^2
+x<-matrix(c(rep(1,24),empleados1$Altura),ncol = 2,nrow = 24)
+Cjj<-solve(t(x)%*%x)
+B0=-99.0330
+B1=0.9778
+alfa=0.05
+#Intervalo para B0:
+intervalo<-c(B0+qt(alfa/2,24-2)*sqrt(Cjj[1,1]*sigma2),B0-qt(alfa/2,24-2)*sqrt(Cjj[1,1]*sigma2))
+#Intervalo para B1:
+intervalo1<-c(B1+qt(alfa/2,24-2)*sqrt(Cjj[2,2]*sigma2),B1-qt(alfa/2,24-2)*sqrt(Cjj[2,2]*sigma2))
+
+
+#punto 4 y 5:
 #Vamos a representar los hombres con 1 y las mujeres con 0
 library("car")
 sexocodificado<- recode(empleados1$Sexo, '"Hombre"=1; "Mujer"=0; ;', as.factor.result=TRUE)
